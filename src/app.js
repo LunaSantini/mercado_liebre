37df412213d748a470fce6ae2b81346ae23ae377
LunaSantini <lunasantinit@gmail.com>
2022-12-15 21:17:29 -0300

    Formulario registro

--- src/app.js
+++ src/app.js
@@ -7,8 +7,13 @@ app.use(express.static('public'));
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log('Server runing in http://localhost:'  + port));
-
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+});
+
+app.listen(port, () => console.log('Server runing in http://localhost:'  + port));
+
